03c8627da901e9b7157d541207d82996b614103c
Nurjahonbek <nurjahonbekweb@gmail.com>
2024-11-14 00:26:34 +0500

    .

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./*.html"],
+    theme: {
+      extend: {
+        colors:{
+          'dark-green':'#079211',
+          'costum-gray':'#808080'
+        },
+        fontFamily:{
+          'inter': ['Inter', 'sans-serif']
+        },
+        spacing:{
+          '4.5':'17px'
+        }
+      }
+    },
+    plugins: [],
+  }
+  
